7a3e77005d0a1c794c44c8bae677b628bda1e466
dnagirniy <den.nagirniy@gmail.com>
2016-05-24 17:18:09 +0300

    new branch

--- SpringMVC/src/main/java/lgs/entity/Author.java
+++ SpringMVC/src/main/java/lgs/entity/Author.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-//ciu
+//jrs
 @Entity
 public class Author {
 	@Id
